69790073113b32ad4f842fe772d07a0f01b2e1e8
Sagimune <50073535+Sagimune@users.noreply.github.com>
2022-12-03 12:53:01 +0800

    Update compression.h

--- HuffmanCompression/compression.h
+++ HuffmanCompression/compression.h
@@ -2,11 +2,39 @@
 #define COMPRESSION_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QMap>
+#include <QFile>
+#include <queue>
 
-class Compression
+struct Node
 {
+    QChar C;
+    int weight;
+    Node *L,*R;
+    bool leaf;
+};
+struct CMP
+{
+    bool operator() (Node* A,Node *B)
+    {
+        return A->weight > B->weight;
+    }
+};
+
+class Compression : public QMainWindow
+{
+private:
+    QMap<QChar,int> weightmap;
+    std::priority_queue<Node*,std::vector<Node*>,CMP> container;
 public:
     Compression();
+protected:
+    void DEL(Node* root);
+    void Weightmap_Init(QFile& in);
+    Node *MakeNewNode(QChar ch);
+    void Container_Init();
+    void HuffmanTree_Init();
 };
 
 #endif // COMPRESSION_H
