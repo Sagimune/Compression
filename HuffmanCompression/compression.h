eea35368e18c1ab3f819265471780b0987d412f0
Sagimune <50073535+Sagimune@users.noreply.github.com>
2022-12-03 14:44:27 +0800

    Update compression.h

--- HuffmanCompression/compression.h
+++ HuffmanCompression/compression.h
@@ -2,6 +2,7 @@
 #define COMPRESSION_H
 
 #include <QMainWindow>
+#include <QMessageBox>
 #include <QVector>
 #include <QMap>
 #include <QFile>
@@ -27,14 +28,18 @@ class Compression : public QMainWindow
 private:
     QMap<QChar,int> weightmap;
     std::priority_queue<Node*,std::vector<Node*>,CMP> container;
+    QMap<QChar,QString> passwordmap;
 public:
     Compression();
+    void Zip(QString path);
+    void UnZip(QString path);
 protected:
     void DEL(Node* root);
     void Weightmap_Init(QFile& in);
     Node *MakeNewNode(QChar ch);
     void Container_Init();
     void HuffmanTree_Init();
+    void ZipPassword_Init(Node *root, QString password);
 };
 
 #endif // COMPRESSION_H
