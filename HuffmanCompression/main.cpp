1541e51f819790067953a19bfcfb1ecfb962f270
Sagimune <50073535+Sagimune@users.noreply.github.com>
2022-12-05 16:34:57 +0800

    Update main.cpp

--- HuffmanCompression/main.cpp
+++ HuffmanCompression/main.cpp
@@ -3,15 +3,10 @@
 #include <QApplication>
 #include "compression.h"
 
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    Compression TT;
-    TT.Zip("D:/tmp/qwq.txt");
-    Compression T;
-    T.UnZip("D:/tmp/qwq.txt.huffmanzip");
     w.show();
     return a.exec();
 }
